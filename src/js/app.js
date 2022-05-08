c3c9251b977f29640da5666861d357c9d485bba5
Andrii <andrii.vytak@gmail.com>
2022-05-08 15:20:41 +0300

    Update perfomance

--- src/js/app.js
+++ src/js/app.js
@@ -6,24 +6,34 @@ const perfomanceIcons = document.querySelectorAll('.card');
 const airPlane = document.querySelector('.perfomance__airplane');
 const luxury = document.querySelector('.luxury');
 const memberShip = document.querySelector('.membership');
-
 const lazyImages = document.querySelectorAll('img[data-src]');
-let lazyImagesPositions = [];
+const departureDate = document.querySelector('input[name="datepicker-departure"]');
+const returnDate = document.querySelector('input[name="datepicker-return"]');
+const oneDay = 86400000;
+const prev = document.querySelectorAll('.booking__prev');
+const next = document.querySelectorAll('.booking__next');
 const windowHeight = document.documentElement.clientHeight;
+const bookingArrows = document.querySelectorAll('.booking__arrow');
+const bookingHeaders = document.querySelectorAll('.booking__link-header');
+const header = document.querySelector('.header');
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+const burgerIcon = document.querySelector('.menu__icon');
+const burgerMenu = document.querySelector('.menu__body');
 
+let lazyImagesPositions = [];
 
-
+// Lazy image loading
 if (lazyImages.length > 0) {
     lazyImages.forEach(img => {
         if (img.dataset.src) {
-            lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+            lazyImagesPositions.push(img.getBoundingClientRect().top + window.scrollY);
         }
     });
 }
 
 function lazyScrollCheck() {
     let imgIndex = lazyImagesPositions.findIndex(
-        item => pageYOffset > item - windowHeight
+        item => window.scrollY > item - windowHeight
     );
 
     if (imgIndex >= 0) {
@@ -36,15 +46,15 @@ function lazyScrollCheck() {
     }
 }
 
+// Revert destinations for fly
 buttonRevert.addEventListener('click', () => {
     [destinations[0].innerHTML, destinations[1].innerHTML] = [destinations[1].innerHTML, destinations[0].innerHTML];
 
 });
 
+// Animation for lazy loading
 
-window.addEventListener('scroll', () => {
-    lazyScrollCheck();
-    fixedHeader()
+function animationLazyImages() {
     if (window.scrollY > 250) {
         booking.classList.add('visible');
     }
@@ -77,109 +87,64 @@ window.addEventListener('scroll', () => {
     if (window.scrollY > 3140) {
         memberShip.classList.add('lazy');
     }
-});
-
+}
 // Datepicker
-const departureDate = document.querySelector('input[name="datepicker-departure"]');
-const returnDate = document.querySelector('input[name="datepicker-return"]');
-let prev = document.querySelectorAll('.booking__prev');
-let next = document.querySelectorAll('.booking__next');
 
 const departureDatePicker = new Datepicker(departureDate, {
     autohide: true,
     format: 'D dd M',
     orientation: 'left',
     todayHighlight: true,
-
     beforeShowDay(date) {
-        if (date < new Date()) {
+        if (date < new Date() - oneDay ) {
             return false;
         }
     }
 });
 
-
 const returnDatePicker = new Datepicker(returnDate, {
     autohide: true,
     format: 'D dd M',
     orientation: 'left',
-    todayHighlight: true
+    todayHighlight: true,
+
+    beforeShowDay(date) {
+        if (date < new Date() - oneDay ) {
+            return false;
+        }
+    }
 });
 
-
-// updateData = (dateInput, datapicker, index) => {
-//     dateInput.addEventListener('changeDate', () => {
-//         datapicker.dates.forEach(el => {
-//             prev[index].addEventListener('click', () => {
-//                 // if (el >= new Date()) {
-//                 el = el - 86400000;
-//                 datapicker.setDate(el);
-//                 // }
-//             });
-//             next[index].addEventListener('click', () => {
-//                 el = el + 86400000;
-//                 datapicker.setDate(el);
-//             });
-//         });
-
-//     });
-// };
-
-
-departureDate.addEventListener('changeDate', () => {
+departureDate.addEventListener('changeDate', () => {  
     departureDatePicker.dates.forEach(el => {
-
-
-
         prev[0].addEventListener('click', () => {
             if (el >= new Date()) {
-                el = el - 86400000;
+                el = el - oneDay;
                 departureDatePicker.setDate(el);
             }
         });
-
         next[0].addEventListener('click', () => {
-            el = el + 86400000;
+            el = el + oneDay;
             departureDatePicker.setDate(el);
         });
-
-
-
-
     });
 });
 
-
-
-
-
-
-
 returnDate.addEventListener('changeDate', () => {
-
     returnDatePicker.dates.forEach(el => {
-
         prev[1].addEventListener('click', () => {
             if (el >= Date.now()) {
-                el = el - 86400000;
+                el = el - oneDay;
                 returnDatePicker.setDate(el);
             }
         });
-
         next[1].addEventListener('click', () => {
-            el = el + 86400000;
+            el = el + oneDay;
             returnDatePicker.setDate(el);
         });
 
     })
 });
-
-
-console.log('Hello')
-// updateData(departureDate, departureDatePicker, 0);
-// updateData(returnDate, returnDatePicker, 1);
-
-
 
 // Check for touch screen
 
@@ -209,9 +174,6 @@ const isMobile = {
     }
 };
 
-let bookingArrows = document.querySelectorAll('.booking__arrow');
-let bookingHeaders = document.querySelectorAll('.booking__link-header');
-
 if (isMobile.any()) {
     document.body.classList.add('_touch')
 
@@ -232,10 +194,7 @@ if (isMobile.any()) {
     document.body.classList.add('_pc')
 }
 
-
 // Fixed header
-
-const header = document.querySelector('.header');
 
 if (screen.width > 767) {
     document.addEventListener('scroll', () => {
@@ -245,7 +204,6 @@ if (screen.width > 767) {
             header.classList.remove('_fixed')
         }
     })
-
 }
 
 function fixedHeader() {
@@ -263,14 +221,10 @@ function fixedHeader() {
         }
     }
 }
-// console.log(screen.width > 763)
 
-// Burger
+// Burger menu
 
-const burgerIcon = document.querySelector('.menu__icon');
-const burgerMenu = document.querySelector('.menu__body');
 if (burgerIcon) {
-
     burgerIcon.addEventListener('click', () => {
         document.body.classList.toggle('_lock');
         burgerIcon.classList.toggle('_active');
@@ -280,40 +234,34 @@ if (burgerIcon) {
 
 // Scroll for sections
 
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-
 if (menuLinks.length > 0) {
     menuLinks.forEach(el => {
         el.addEventListener('click', onMenuClickLink)
     });
-
 
     function onMenuClickLink(e) {
         const menuLink = e.target;
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const goToBlock = document.querySelector(menuLink.dataset.goto);
             const goToBlockValue = goToBlock.getBoundingClientRect().top + scrollY - header.offsetHeight;
-
             if (burgerIcon.classList.contains('_active')) {
                 document.body.classList.remove('_lock');
                 burgerIcon.classList.remove('_active');
                 burgerMenu.classList.remove('_active');
             }
-
             window.scrollTo({
                 top: goToBlockValue,
                 behavior: "smooth"
             });
-
             e.preventDefault();
         }
     }
 }
 
+animationLazyImages();
+window.addEventListener('scroll', () => {
+    lazyScrollCheck();
+    fixedHeader()
+    animationLazyImages();
+});
 
-const regex = /[+]{0,1}[0-9]{0,12}/;
-
-console.log(regex.test('359848'));
-console.log(regex.test('359848%%$$'));
-console.log(regex.test('359848++'));
-console.log(regex.test('++359848++'));
